38ad39a6beeca127a7b3cb62c904b7f02bc57930
Arunanshu Biswas <mydellpc07@gmail.com>
2024-06-13 21:57:01 +0530

    switch to embassy as executor

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
 use esp_backtrace as _;
 use esp_hal::{peripherals::Peripherals, prelude::*, system::SystemControl};
 use esp_println::println;
 
-#[entry]
-fn main() -> ! {
+#[main]
+async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     let _system = SystemControl::new(peripherals.SYSTEM);
 
