8d4366a2adf3b96ba4e89297b34c7d05ef3aed4e
Arunanshu Biswas <mydellpc07@gmail.com>
2024-06-13 23:36:44 +0530

    feat: create timer and clocks

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,29 @@
 #![no_main]
 
 use embassy_executor::Spawner;
+use embassy_time::Timer;
 use esp_backtrace as _;
-use esp_hal::{peripherals::Peripherals, prelude::*, system::SystemControl};
+use esp_hal::{
+    clock::ClockControl, peripherals::Peripherals, prelude::*, system::SystemControl,
+    timer::systimer::SystemTimer,
+};
 use esp_println::println;
 
 #[main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let peripherals = Peripherals::take();
-    let _system = SystemControl::new(peripherals.SYSTEM);
+    // initialize the peripherals
+    let system = SystemControl::new(peripherals.SYSTEM);
+    let clock = ClockControl::boot_defaults(system.clock_control).freeze();
+    let timer = SystemTimer::new_async(peripherals.SYSTIMER); // async because embassy needs async
 
-    println!("Hello world!");
+    esp_hal_embassy::init(&clock, timer);
 
-    loop {}
+    let mut x = 0;
+    // TODO: somehow enable the clock?
+    loop {
+        println!("Hello, world! x = {}", x);
+        Timer::after_millis(500).await;
+        x += 1;
+    }
 }
