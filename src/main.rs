4d8076d1fcf333f77c3af4fe5a98fe1d1651864d
Arunanshu Biswas <mydellpc07@gmail.com>
2024-06-14 20:04:36 +0530

    feat(aes): simple aes and sha demonstration plus channels

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
 #![no_std]
 #![no_main]
 
+mod xchg;
+
 use embassy_executor::Spawner;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel};
 use embassy_time::Timer;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, peripherals::Peripherals, prelude::*, system::SystemControl,
+    aes::{Aes, Mode},
+    clock::ClockControl,
+    peripherals::Peripherals,
+    prelude::*,
+    rng::Rng,
+    sha::{Sha, ShaMode},
+    system::SystemControl,
     timer::timg::TimerGroup,
 };
 use esp_println::println;
+use heapless::{String, Vec};
+use xchg::XchgPipeTx;
+
+static SHARED: Channel<CriticalSectionRawMutex, (&'static str, u32), 8> = Channel::new();
+
+#[embassy_executor::task]
+async fn producer(tx: XchgPipeTx<'static, (&'static str, u32), 8>) {
+    let mut x = 0;
+    loop {
+        tx.send(("producer1", x)).await;
+        Timer::after_millis(200 + x as u64).await;
+        x += 1;
+    }
+}
+
+#[embassy_executor::task]
+async fn producer2(tx: XchgPipeTx<'static, (&'static str, u32), 8>) {
+    let mut x = 0;
+    loop {
+        tx.send(("producer2", x)).await;
+        Timer::after_millis(300 + x as u64).await;
+        x += 1;
+    }
+}
+
+#[embassy_executor::task]
+async fn producer3(tx: XchgPipeTx<'static, (&'static str, u32), 8>) {
+    for i in (0..10).rev() {
+        tx.send(("producer3", i)).await;
+        Timer::after_millis(200).await;
+    }
+    tx.send(("producer3 exiting", 0)).await;
+}
 
 #[main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let peripherals = Peripherals::take();
     // initialize the peripherals
     let system = SystemControl::new(peripherals.SYSTEM);
@@ -20,11 +62,43 @@ async fn main(_spawner: Spawner) {
 
     esp_hal_embassy::init(&clock, timer);
 
-    let mut x = 0;
-    // TODO: somehow enable the clock?
+    // create an RNG instance
+    let mut rng = Rng::new(peripherals.RNG);
+
+    // create a key buffer and fill it
+    let mut key = [0u8; 32];
+    rng.read(&mut key);
+    println!("Key: {:?}", key);
+
+    // create a string buffer
+    let mut buf = *b"string buffer 19";
+
+    // encrypt and decrypt
+    let mut aes = Aes::new(peripherals.AES);
+    aes.process(&mut buf, Mode::Encryption256, key);
+    println!("AES[enc]: {:?}", buf);
+    aes.process(&mut buf, Mode::Decryption256, key);
+    println!("AES[dec]: {:?}", buf);
+
+    // can we get back the string?
+    let buf_as_vec = Vec::<_, 16>::from_slice(&buf).unwrap();
+    let buf_as_str = String::<16>::from_utf8(buf_as_vec).unwrap();
+    println!("decoded AES[dec]: {}", buf_as_str);
+
+    // compute hash
+    let mut sha = Sha::new(peripherals.SHA, ShaMode::SHA256, None);
+    sha.update(&buf).ok();
+    sha.finish(&mut buf).ok();
+    println!("SHA: {:?}", buf);
+
+    let (tx, rx) = xchg::channel(&SHARED);
+
+    spawner.spawn(producer(tx)).unwrap();
+    spawner.spawn(producer2(tx)).unwrap();
+    spawner.spawn(producer3(tx)).unwrap();
+
     loop {
-        println!("Hello, world! x = {}", x);
-        Timer::after_millis(500).await;
-        x += 1;
+        let (produer, value) = rx.recv().await;
+        println!("[{}] produced value = {}", produer, value);
     }
 }
---
+++ src/xchg.rs
@@ -0,0 +1,49 @@
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex,
+    channel::{Receiver, Sender},
+};
+
+type RawMutexSender<'channel, T, const N: usize> = Sender<'channel, CriticalSectionRawMutex, T, N>;
+
+type RawMutexReceiver<'channel, T, const N: usize> =
+    Receiver<'channel, CriticalSectionRawMutex, T, N>;
+
+#[derive(Clone, Copy)]
+pub struct XchgPipeTx<'channel, T: Sized, const N: usize> {
+    sender: RawMutexSender<'channel, T, N>,
+    // other fields
+}
+
+impl<'channel, T: Sized, const N: usize> XchgPipeTx<'channel, T, N> {
+    pub async fn send(&self, value: T) {
+        self.sender.send(value).await;
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct XchgPipeRx<'channel, T: Sized, const N: usize> {
+    reciever: RawMutexReceiver<'channel, T, N>,
+    // other fields
+}
+
+impl<'channel, T: Sized, const N: usize> XchgPipeRx<'channel, T, N> {
+    pub async fn receive(&self) -> T {
+        self.reciever.receive().await
+    }
+
+    pub async fn recv(&self) -> T {
+        self.receive().await
+    }
+}
+
+pub fn channel<'a, T, const N: usize>(
+    channel: &'a embassy_sync::channel::Channel<CriticalSectionRawMutex, T, N>,
+) -> (XchgPipeTx<'_, T, N>, XchgPipeRx<'_, T, N>) {
+    let tx = XchgPipeTx {
+        sender: channel.sender(),
+    };
+    let rx = XchgPipeRx {
+        reciever: channel.receiver(),
+    };
+    (tx, rx)
+}
